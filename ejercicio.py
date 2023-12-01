95f4a2443fadc5550bd7b49c92f454d69379c3ad
UnaiMunoz <unaimunoz2024@gmail.com>
2023-12-01 16:41:34 +0100

    ejercicio

---
+++ ejercicio.py
@@ -0,0 +1,12 @@
+def calcular_area_rectangle(base, altura):
+    return base * altura
+
+base = 3
+altura = 4
+area = calcular_area_rectangle(base, altura)
+print(f"L'area d'un rectangle amb base {base} i {altura} altura és {area}")
+
+base = 2
+altura = 3
+area = calcular_area_rectangle(base, altura)
+print(f"L'area d'un rectangle amb base {base} i {altura} altura és {area}")
